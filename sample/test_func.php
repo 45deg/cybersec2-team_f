3ce43f215458c00fb6af612f8f7d16a65401e7c9
45deg <45deg@users.noreply.github.com>
2017-11-20 14:25:59 +0900

    Added sample

--- sample/test_func.php
+++ sample/test_func.php
@@ -4,6 +4,14 @@ function clean_func(){
   return 'echo clean';
 }
 
+function partial_dirty(){
+  if($_GET['x'] > 1){
+    return $_GET['x'];
+  } else {
+    return 'clean';
+  }
+}
+
 function get_func($name){
   return $_GET[$name];
 }
@@ -15,8 +23,11 @@ function destroy_c(){
 
 $a = get_func('hoge');
 system($a); // BAD
+
 $b = clean_func();
 system($b); // OK
+
+system(partial_dirty()); // BAD
 
 $c = 'clean!?';
 destroy_c();
